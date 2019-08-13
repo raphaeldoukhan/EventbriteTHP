4e3ef21ff635797242b1bf4dacfe556b6d74471b
raphaeldoukhan <raphael.doukhan@live.fr>
2019-08-13 15:54:07 +0200

    v2

--- Gemfile
+++ Gemfile
@@ -64,3 +64,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'faker', '~> 1.4', '>= 1.4.3'
 gem 'table_print', '~> 1.5', '>= 1.5.6'
 gem 'devise', '~> 4.2'
+gem 'bootstrap', '~> 4.3', '>= 4.3.1'
+gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
     validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
-    validates :description, presence: true
-    validates :first_name, presence: true
-    validates :last_name, presence: true
+    #validates :description, presence: true
+    #validates :first_name, presence: true
+    #validates :last_name, presence: true
     has_many :attendances
     has_many :events, through: :attendances
     has_many :event_admin, foreign_key: 'administrator_id', class_name: "Event"
